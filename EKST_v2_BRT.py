ec90717bf22b710cae3cd43c2395899aa8846c28
Jakub Sadilek <jakub.sadilek@gmail.com>
2026-03-17 12:55:01 +0100

    labels in brt

--- EKST_v2_BRT.py
+++ EKST_v2_BRT.py
@@ -3,20 +3,22 @@ import gdsfactory as gf
 from ekin_master_die import ekn_master_die_ds, edge_coupler_array_ekn_def
 from test_crosssections import xs_ekn300_te_IMGREV
 
+label_txt = gf.partial(gf.components.text_rectangular, layer = "GE")
+
 def ekst_v2_brt_master(
         master_die: gf.typings.ComponentSpec = ekn_master_die_ds,
         widths: tuple = (0.75, 1, 1.25, 1.5),
         bend_rads: tuple = (300,400,500,600,700,800,1000),
         cross_section:gf.typings.CrossSectionSpec = xs_ekn300_te_IMGREV,
         ec_array_def: gf.typings.ComponentSpec = edge_coupler_array_ekn_def,
-        label_layer: gf.typings.LayerSpecs = (5,0)
+        label_txt: gf.typings.ComponentSpec = label_txt
         
 ) -> gf.Component:
 
     eca_w1 = ec_array_def(widths = widths)
     eca_e1 = ec_array_def(widths = widths, axis_reflection = True)
 
-    c = ekn_master_die_ds(
+    c = master_die(
         fiber_arrays_by_side={
         "W": [eca_w1],
         "E": [eca_e1],
@@ -42,13 +44,12 @@ def ekst_v2_brt_master(
 
     routes = []
     start_offset = 13.5 * ec_pitch # what a magic constant :D :D 
-
+    lbl_offset = ec_array_def().settings["text_offset"]
 
     for i in range(0, len(bend_rads)):
         for x in range(0, len(widths)):
             offset = int(start_offset - (i*len(widths)+x)*ec_pitch)
-            routes.append(
-                gf.routing.route_single(
+            route = gf.routing.route_single(
                     component=c,
                     cross_section=cross_section,
                     port1=ports1[i*len(widths)+x],
@@ -57,7 +58,14 @@ def ekst_v2_brt_master(
                     waypoints=((offset,ports1[i*len(widths)+x].dcenter[1]),(offset, ports2[i*len(widths)+x].dcenter[1]),),
                     bend=ekn_bend(radius=bend_rads[i])
                 )
-            )
+            
+            print(bend_rads[i], widths[x], route.length)
+
+            if label_txt != None:
+                txt = c.add_ref(label_txt(text="    W{:.2f}um L{:.3f}mm".format(route.start_port.dwidth, route.length/1e6)))       #in mm
+                txt.dmove(origin=(0,0), destination=(route.start_port.trans.disp.x/1000 + lbl_offset[0], route.start_port.trans.disp.y/1000 + lbl_offset[1]))
+
+            routes.append(route)
 
 #TODO: This is plain hack ... if there would be odd number of al. loops it would fall apart
     for arr in c.info['fiber_arrays']:
@@ -78,12 +86,6 @@ def ekst_v2_brt_master(
                 route_width=c.ports.filter(regex=rex0)[0].width,
                 #separation= 127
                                         )
-
-
-    #print(routes[0].length)
-    # print(al)length
-
-    print(c.info['fiber_arrays'])
 
     return c
 
