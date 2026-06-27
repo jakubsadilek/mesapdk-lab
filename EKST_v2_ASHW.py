0b76fe6d7f8cee77a9172e5ce00ddd713515a0a1
ekinbircann <ekin.bosdurmaz@utwente.nl>
2026-06-27 14:31:53 +0200

    Logos and final version before LOTR tapeout

--- EKST_v2_ASHW.py
+++ EKST_v2_ASHW.py
@@ -1,22 +1,22 @@
 import gdsfactory as gf
 
-from ekin_master_die import edge_coupler_array_ekn_def, edge_coupler_array_ekn_def_centerskip, edge_coupler_array_ekn_def_butt
-from cross_sections import xs_ekn300_te_IMGREV
-from ekin_master_die import ekn_master_die_ss_xl, ekn_master_die_ds_xl, ekn_master_die_ds
+from ekin_master_die import ekn_master_die_ds_xl, edge_coupler_array_ekn_def, edge_coupler_array_ekn_def_centerskip, edge_coupler_array_ekn_def_butt
 
-label_txt = gf.partial(gf.components.text_rectangular, layer = "LABEL_SIN")
+from cross_sections import xs_ekn300_te_IMGREV
+
+label_txt = gf.partial(gf.components.text_rectangular, layer = "GE")
 
 
 @gf.cell_with_module_name
-def ekst_v2_brt_master(
-        master_die: gf.typings.ComponentSpec = ekn_master_die_ds,
-        widths: tuple = (0.75, 1, 1.25, 1.5),
-        bend_rads: tuple = (7500),
+def ekst_v2_uwwg_master(
+        master_die: gf.typings.ComponentSpec = ekn_master_die_ds_xl,
+        widths: tuple = (50, 60,70,80,50,60,70,80,50,60,70,80,50,60), 
+        bend_rads: tuple = None,
         cross_section:gf.typings.CrossSectionSpec = xs_ekn300_te_IMGREV,
         ec_array_def: gf.typings.ComponentSpec = edge_coupler_array_ekn_def,
         label_txt: gf.typings.ComponentSpec = label_txt,
-        label: str = "EKAJ_v0\nBRT",
-        chip_id_label: str = "EKAJ_v0 BRT\nW00_I00\nX20.0 Y20.0",
+        label: str = "EKAJ_v0\nUWWG",
+        chip_id_label: str = "EKAJ_v0 UWWG\nW00_I00\nX20.0 Y20.0",
         ext_grp_spacing: float = 0,
         logo: gf.typings.ComponentSpec = None,
         logo_loc: gf.typings.Position = None,        
@@ -24,20 +24,21 @@ def ekst_v2_brt_master(
     
     d = gf.Component()
 
-    eca_w1 = ec_array_def(widths = widths)
-    eca_e1 = ec_array_def(widths = widths, axis_reflection = True)
+    eca_w1 = ec_array_def(widths = widths, nc_ports = [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28 ], text_offset = (0,75))
+    eca_e1 = ec_array_def(widths = widths, nc_ports = [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28 ], text_offset = (0,75), axis_reflection = True)
 
     md = d.add_ref(master_die(
         fiber_arrays_by_side={
         "W": [eca_w1],
         "E": [eca_e1],
-        }
+        },
+        
     ))
     #c.locked = False
 
         
     ec_available = len(md.cell.info["fiber_arrays"][0]["fa_usable_channel_indices"])
-    req_connections = len(widths)*len(bend_rads)
+    req_connections = len(widths)
     ec_pitch = md.cell.info["fiber_arrays"][0]["fa_pitch"]
 
     if req_connections > ec_available:
@@ -51,37 +52,37 @@ def ekst_v2_brt_master(
     #print(ports1)
     
 
-    ekn_bend=gf.partial(gf.c.bend_euler, cross_section=xs_ekn300_te_IMGREV)
+    # ekn_bend=gf.partial(gf.c.bend_euler, cross_section=xs_ekn300_te_IMGREV)
 
     routes = []
 
     # TODO: fix start offset to calculate how many ports are utilized and then decide where to go. 
-    start_offset = (len(widths)*len(bend_rads)-1)/2 * ec_pitch + (len(bend_rads)-1)/2 * ext_grp_spacing
+    # start_offset = (len(widths)*len(bend_rads)-1)/2 * ec_pitch + (len(bend_rads)-1)/2 * ext_grp_spacing
 
     #start_offset = 13.5 * (ec_pitch) # what a magic constant :D :D 
     lbl_offset = ec_array_def().settings["text_offset"]
 
-    for i in range(0, len(bend_rads)):
+    # for i in range(0, len(bend_rads)):
 
-        for x in range(0, len(widths)):
-            offset = int(start_offset - (i*len(widths)+x)*(ec_pitch)-ext_grp_spacing*i)
-            route = gf.routing.route_single(
+    for x in range(0, len(widths)):
+            # offset = int(start_offset - (i*len(widths)+x)*(ec_pitch)-ext_grp_spacing*i)
+        route = gf.routing.route_single(
                     component=d,
                     cross_section=cross_section,
-                    port1=ports1[i*len(widths)+x],
-                    port2=ports2[i*len(widths)+x],
-                    route_width=ports1[i*len(widths)+x].width,
-                    waypoints=((offset,ports1[i*len(widths)+x].dcenter[1]),(offset, ports2[i*len(widths)+x].dcenter[1]),),
-                    bend=ekn_bend(radius=bend_rads[i], width = widths[x])
+                    port1=ports1[x],
+                    port2=ports2[x],
+                    route_width=ports1[x].width,
+                    # waypoints=((offset,ports1[i*len(widths)+x].dcenter[1]),(offset, ports2[i*len(widths)+x].dcenter[1]),),
+                    # bend=ekn_bend(radius=bend_rads[i])
                 )
             
             #print(bend_rads[i], widths[x], route.length)
 
-            if label_txt != None:
-                txt = d.add_ref(label_txt(text="W{:.2f}um L{:.3f}mm".format(route.start_port.dwidth, route.length/1e6)))       #in mm
-                txt.dmove(origin=(0,0), destination=(route.start_port.trans.disp.x/1000 + lbl_offset[0] - 850, route.start_port.trans.disp.y/1000 + lbl_offset[1]))
+        if label_txt != None:
+            txt = d.add_ref(label_txt(text="W{:.2f}um L{:.3f}mm".format(route.start_port.dwidth, route.length/1e6)))       #in mm
+            txt.dmove(origin=(0,0), destination=(route.start_port.trans.disp.x/1000 + lbl_offset[0] - 850, route.start_port.trans.disp.y/1000 + lbl_offset[1]+50))
 
-            routes.append(route)
+        routes.append(route)
 
 #TODO: This is plain hack ... if there would be odd number of al. loops it would fall apart
     for arr in md.cell.info['fiber_arrays']:
@@ -107,7 +108,7 @@ def ekst_v2_brt_master(
     # -------------------------------------------------------------------------
 
     if label != None:
-        tag = d.add_ref(label_txt(size=100, text=label)).drotate(90).dmove(origin=(0,0), destination=(-9250, 600))
+        tag = d.add_ref(label_txt(size=100, text=label)).dmove(origin=(0,0), destination=(0, 3000))
     if chip_id_label != None:
         chip_id_tag = d.add_ref(label_txt(size=30, text=chip_id_label, justify = "center")).dmove(origin=(0,0), destination=(8550, -4350))
 
@@ -139,9 +140,9 @@ if __name__ == "__main__":
         )
 
     #ekst_v2_brt_master(ext_grp_spacing=127).show()
-    ekst_v2_brt_master(ext_grp_spacing=127, 
-                       ec_array_def=edge_coupler_array_ekn_def_butt, 
-                       logo=logo, logo_loc=(-5000,5000),
-                       widths=(9,10,11,12),
-                       bend_rads=(2000,2000)).show()
-    #ekst_v2_brt_master(bend_rads=(2000,1000), widths=(2,4,6,8,2,4,6,8,2,4,6,8),ext_grp_spacing=512, label="EKST_v2\nMMWG", ec_array_def=edge_coupler_array_ekn_def_centerskip).show()
+    ekst_v2_uwwg_master(ext_grp_spacing=127, 
+                        ec_array_def=edge_coupler_array_ekn_def_butt, 
+                        logo=logo, 
+                        logo_loc=(0,-3000)).plot()
+    # ekst_v2_uwwg_master(bend_rads=(2000,1000), widths=(2,4,6,8,2,4,6,8,2,4,6,8),ext_grp_spacing=512, label="EKST_v2\nMMWG", ec_array_def=edge_coupler_array_ekn_def_centerskip).show()
+
